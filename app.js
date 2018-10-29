fc4baba391fc837c9c9353c2ac0e7e5e3982b619
Saabbir Hossain <thesaabbir@gmail.com>
2018-10-30 03:12:04 +0600

    Update UI

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ function validateForm(e, rules) {
             const userNameVal = userNameEl.value.trim().toLowerCase()
 
             if (userNameVal === '') {
-                errors.push('The name field is required. You can\'t leave it empty.')
+                errors.push('Username is <strong>required</strong>. You can\'t leave it empty.')
                 addErrorField(userNameEl)
             } else if (!isNaN(userNameVal.charAt(0))) {
                 errors.push(`Username can't start with a number ${userNameVal.charAt(0)}`)
@@ -58,24 +58,27 @@ function validateForm(e, rules) {
         if (rules.userEmail.required) {
             const userEmailEl = form.elements.userEmail
             const userEmailVal = userEmailEl.value.trim().toLowerCase()
-            const userEmailName = userEmailVal.slice(0, userEmailVal.indexOf('@'));
-            const userEmailDomain = userEmailVal.slice(userEmailVal.indexOf('@') + 1);
-            const validEmailDomains = ['gmail.com', 'yahoo.com', 'outlook.com', 'aol.com'];
-            const slangWords = ['LOL', 'OMG', 'WTF', 'LMAO', 'RIP', 'FUCK'];            
+            const userEmailName = userEmailVal.slice(0, userEmailVal.indexOf('@'))
+            const userEmailDomain = userEmailVal.slice(userEmailVal.indexOf('@') + 1)
+            const validEmailDomains = ['gmail.com', 'yahoo.com', 'outlook.com', 'aol.com']
+            const slangWords = ['LOL', 'OMG', 'WTF', 'LMAO', 'RIP', 'FUCK']
+            const searchSlangWord = slangWords.filter(function(slangWord) {
+                return userEmailName.includes(slangWord.toLowerCase())
+            })            
             const emailRegExp = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
             const isEmailValid = emailRegExp.test(userEmailVal)
             
             if (userEmailVal === '') {
-                errors.push('The email field is required. You can\'t leave it empty.')
+                errors.push('Email address is <strong>mandatory</strong>. You can\'t leave it empty.')
                 addErrorField(userEmailEl)
             } else if (!isEmailValid) {
                 errors.push(`<strong>${userEmailVal}</strong> is not a valid email address.`)
                 addErrorField(userEmailEl)
-            } else if (validEmailDomains.indexOf(userEmailDomain) === -1) {
+            } else if (!validEmailDomains.includes(userEmailDomain)) {
                 errors.push(`<strong>${userEmailDomain}</strong> is not a valid email domain.`)
                 addErrorField(userEmailEl)
-            } else if (slangWords.indexOf(userEmailName.toUpperCase()) !== -1) {
-                errors.push(`Your user name <strong>${userEmailName}</strong> contains slang word.`)
+            } else if (searchSlangWord.length) {
+                errors.push(`Your email username contains slang word <strong>${searchSlangWord.join(', ')}</strong>. Which we do not accept.`)
                 addErrorField(userEmailEl)
             }
         }
@@ -87,7 +90,9 @@ function validateForm(e, rules) {
     errorFields.forEach(field => field.style.borderColor = 'red')
 
     errorMsg.innerHTML = errors.join('<br>')
-    form.appendChild(errorMsg)
+    const boxFooter = document.querySelector('.box__footer')
+    boxFooter.parentNode.insertBefore(errorMsg, boxFooter)
+    //form.appendChild(errorMsg)
 
     if (errors.length !== 0) {
         e.preventDefault()
